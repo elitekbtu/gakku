c1527c507a6856d99029e5bd5f254c0baf7febe6
elitekbtu <satbaldievst@mail.ru>
2025-04-21 14:08:12 +0500

    new frontend setup

---
+++ frontend/gakku/src/app/components/album-card/album-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-album-card',
+  imports: [],
+  templateUrl: './album-card.component.html',
+  styleUrl: './album-card.component.css'
+})
+export class AlbumCardComponent {
+
+}
